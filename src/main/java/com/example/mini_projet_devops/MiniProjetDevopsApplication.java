e8ca981ade1def21a6ab663ac2a6f7701832148f
imen et emna <fatnassiimen81@gmail.com>
2026-07-04 21:47:50 +0200

    Initial commit: Ajout du projet Maven et Dockerfile

---
+++ src/main/java/com/example/mini_projet_devops/MiniProjetDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_projet_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjetDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_projet_devops/MiniProjetDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_projet_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjetDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
